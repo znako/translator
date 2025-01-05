694f82c7e67ce5ade5c61a29939c4d3053ce74ef
Vasiliy <52681532+vaZZZy1@users.noreply.github.com>
2025-01-06 02:01:31 +0300

    Update app.py

--- services/backend-for-frontend/app.py
+++ services/backend-for-frontend/app.py
@@ -4,31 +4,42 @@ import requests
 
 app = Flask(__name__)
 
+# URL внешнего сервиса
 EXTERNAL_SERVICE_URL = 'TODO: edit'
 
 @app.route('/get-translated-text', methods=['POST'])
 def get_translated_text():
+    max_chars_in_target_lang = 6
     data = request.get_json()
 
-    if not data or 'text' not in data:
-        return jsonify({'error': 'No text provided'}), 400
+    if not data or 'text' not in data or 'target_language' not in data:
+        return jsonify({'error': 'Text or target language not provided'}), 400
 
     text = data['text']
+    target_language = data['target_language']
+
+    if len(target_language) > max_chars_in_target_lang:
+        return jsonify({'error': f'Target language code should not exceed {max_chars_in_target_lang} characters'}), 400
+
+    payload = {
+        'text': text,
+        'target_language': target_language
+    }
 
     try:
-        response = requests.post(EXTERNAL_SERVICE_URL, json={'text': text})
-        response.raise_for_status() 
+        response = requests.post(EXTERNAL_SERVICE_URL, json=payload)
+        response.raise_for_status()
     except requests.RequestException as e:
         return jsonify({"error": "Failed to reach the language detection service", "details": str(e)}), 500
 
     language_data = response.json()
 
-    if 'language' not in language_data:
+    if 'source_language' or 'text' not in language_data:
         return jsonify({'error': 'Invalid response from language detection service'}), 500
 
     result = {
-        'text': text,
-        'language': language_data['language']
+        'text': language_data['text'],
+        'source_language': language_data['source_language']
     }
 
     return jsonify(result)
