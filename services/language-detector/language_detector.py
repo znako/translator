389ddd517b3850a74fb1af7b20af84a6a9c638f8
Arsen Yarullin <arsyarullin@ozon.ru>
2025-01-08 15:50:53 +0300

    make services work together

--- services/language-detector/language_detector.py
+++ services/language-detector/language_detector.py
@@ -1,9 +1,23 @@
+import torch
 from transformers import pipeline
 
 class LanguageDetector:
     def __init__(self):
-        self.detector = pipeline(task="text-classification", model="papluca/xlm-roberta-base-language-detection")
+        print("init language_detector")
+        self.detector = pipeline(
+            task="text-classification",
+            model="papluca/xlm-roberta-base-language-detection",
+            device = -1,
+        )
+        self.detector.model.eval()
+        self._warmup()
+
+    def _warmup(self):
+        try:
+            self.detector("Warmup")
+        except Exception as e:
+            print(f"Error on model initialization: {e}")
 
     def detect_language(self, text):
         result = self.detector(text)[0]
-        return {"language": result['label'], "confidence": result['score']}
+        return {"language": result["label"], "confidence": result["score"]}
