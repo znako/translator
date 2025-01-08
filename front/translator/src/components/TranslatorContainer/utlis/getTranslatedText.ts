372e1277d0e3e9ff6be7f58bd49ca0e7bc876606
Znay Artemiy <artem.znay@mail.ru>
2025-01-09 00:23:12 +0300

    fix api

--- front/translator/src/components/TranslatorContainer/utlis/getTranslatedText.ts
+++ front/translator/src/components/TranslatorContainer/utlis/getTranslatedText.ts
@@ -2,14 +2,30 @@ import { api } from "../../../shared/api/api";
 import {
   GetTranslatedTextRequest,
   GetTranslatedTextResponse,
+  DetectLanguageRequest,
+  DetectLanguageResponse,
 } from "../types/types";
 
-export const getTranslatedText = (text: string, target_language: string) => {
+export const detectLanguage = (text: string) => {
+  return api.post<string, DetectLanguageResponse, DetectLanguageRequest>(
+    "/detect-language",
+    {
+      text,
+    }
+  );
+};
+
+export const getTranslatedText = (
+  text: string,
+  target_language: string,
+  source_language: string
+) => {
   return api.post<string, GetTranslatedTextResponse, GetTranslatedTextRequest>(
     "/get-translated-text",
     {
       text,
       target_language,
+      source_language,
     }
   );
 };
