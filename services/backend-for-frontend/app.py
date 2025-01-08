ba930998eb0693223f45f08e2ff476946ceb0cb1
Arsen Yarullin <arsyarullin@ozon.ru>
2025-01-08 17:27:14 +0300

    fix cors

--- services/backend-for-frontend/app.py
+++ services/backend-for-frontend/app.py
@@ -1,10 +1,12 @@
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+CORS(app)
 
 load_dotenv()
 
